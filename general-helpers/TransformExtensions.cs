d872e1c8500482ae1375d660a9e0ac4c335c7110
bannified <freedomzelda_dc@hotmail.com>
2018-09-13 12:49:50 +0800

    Add TransformExtensions.cs

---
+++ general-helpers/TransformExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TransformExtensions
+{
+    public static void SetLayer(this Transform trans, int layer)
+    {
+        trans.gameObject.layer = layer;
+        foreach (Transform child in trans)
+            child.SetLayer(layer);
+    }
+}
