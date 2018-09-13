eb2f81191815a4eebc4c6aca1d6d1a758cec3be9
bannified <freedomzelda_dc@hotmail.com>
2018-09-13 12:38:28 +0800

    Add GameManager.cs boilerplate.

---
+++ gameplay-essentials/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    static object _lock = new object();
+
+    private static GameManager _gameManager;
+
+    public static GameManager instance
+    {
+        get
+        {
+            if (_gameManager == null)
+            {
+                lock (_lock)
+                    if (_gameManager == null)
+                        _gameManager = new GameManager();
+            }
+            return _gameManager;
+        }
+    }
+
+    #region Monobehavior Functions
+
+    private void Awake()
+    {
+        if (!_gameManager)
+        {
+            _gameManager = FindObjectOfType<GameManager>();
+            if (_gameManager)
+            {
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+        if (instance == null)
+        {
+            _gameManager = this;
+            DontDestroyOnLoad(gameObject);
+        } else
+        {
+            if (instance != this)
+                Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    #endregion
+
+}
